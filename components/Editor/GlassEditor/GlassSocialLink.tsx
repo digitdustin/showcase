a1b5dc6fb404ab7b6d7cf390e38cba1c4aa9ffb2
Dustin <dustinkarp52@gmail.com>
2022-12-24 13:51:21 -0500

    added glass hover

--- components/Editor/GlassEditor/GlassSocialLink.tsx
+++ components/Editor/GlassEditor/GlassSocialLink.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion as m, Reorder } from "framer-motion";
 import { Social } from "../../../constants/editor/types";
-import { Bars3Icon } from "@heroicons/react/24/solid";
 import { useGlassStyleStore } from "../../../stores/useGlassStyleStore";
 
 const GlassSocialLink = ({
@@ -16,36 +15,54 @@ const GlassSocialLink = ({
 }) => {
   const { roundedSocials } = useGlassStyleStore((state) => state);
 
+  const linkRef = useRef<HTMLButtonElement>(null);
+
+  const mouseMoveEvent = (e: MouseEvent) => {
+    if (!linkRef.current) return;
+    const { x, y } = linkRef.current.getBoundingClientRect();
+    linkRef.current.style.setProperty("--x", e.clientX - x);
+    linkRef.current.style.setProperty("--y", e.clientY - y);
+  };
+
+  useEffect(() => {
+    if (linkRef && linkRef.current) {
+      linkRef.current.addEventListener("mousemove", mouseMoveEvent);
+    }
+    // don't forget to *remove* the eventListener
+    // when your component unmounts!
+    return () =>
+      linkRef.current?.removeEventListener("mousemove", mouseMoveEvent);
+  }, [linkRef]);
+
   return (
     <Reorder.Item key={network} value={social}>
-      <m.div layout="position" className="group relative">
-        <m.div
-          style={{
-            background:
-              "linear-gradient( 120deg,rgba( 255, 255, 255, 0.23 ) 10%, rgba( 255, 255, 255, 0.05 ) 100% )",
-            boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
-            backdropFilter: "blur( 13.5px )",
-            WebkitBackdropFilter: "blur( 13.5px )",
-            borderRadius: roundedSocials ? "999px" : "6px",
-            border: "1px solid rgba( 255, 255, 255, 0.18 )",
-          }}
-          className={`relative z-20 flex w-auto cursor-pointer select-none items-center justify-center transition ${
-            roundedSocials ? "rounded-full" : "rounded-md"
-          }`}
-        >
-          <div className={`glass-social relative flex items-center`}>
-            <div className="absolute inset-0 z-20 bg-transparent"></div>
-            <SocialIcon
-              network={network}
-              url={url}
-              bgColor="rgba(0,0,0,0)"
-              fgColor="#ffffff"
-              className="!h-10 !w-10"
-              href={undefined}
-            />
-          </div>
-        </m.div>
-      </m.div>
+      <m.button
+        ref={linkRef}
+        style={{
+          background:
+            "linear-gradient( 120deg,rgba( 255, 255, 255, 0.23 ) 10%, rgba( 255, 255, 255, 0.05 ) 100% )",
+          boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+          backdropFilter: "blur( 13.5px )",
+          WebkitBackdropFilter: "blur( 13.5px )",
+          borderRadius: roundedSocials ? "999px" : "6px",
+          border: "1px solid rgba( 255, 255, 255, 0.18 )",
+        }}
+        className={`shiny relative z-20 flex w-auto cursor-pointer select-none items-center justify-center transition ${
+          roundedSocials ? "rounded-full" : "rounded-md"
+        }`}
+      >
+        <div className={`glass-social relative flex items-center`}>
+          <div className="absolute inset-0 z-20 bg-transparent"></div>
+          <SocialIcon
+            network={network}
+            url={url}
+            bgColor="rgba(255,255,255,0)"
+            fgColor="rgba(255,255,255,0.8)"
+            className="!h-10 !w-10"
+            href={undefined}
+          />
+        </div>
+      </m.button>
     </Reorder.Item>
   );
 };
